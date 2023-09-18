8549317f6b97cb98e54a7be37f00f4f863d5801d
Niko Fotopoulos <fottopoulos@gmail.com>
2023-09-18 20:06:03 +0200

    Add header.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+import styles from './App.module.scss';
+import { ReactComponent as Logo } from './images/HyperComply.svg';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <section>
+          <div className={styles.container}>
+            <header>
+              <nav>
+                <a href="/#" className={styles.logoLink}>
+                  <Logo />
+                </a>
+                <ul>
+                  <li>
+                    <a href="/#">Product</a>
+                  </li>
+                  <li>
+                    <a href="/#">Company</a>
+                  </li>
+                  <li>
+                    <a href="/#">Pricing</a>
+                  </li>
+                  <li>
+                    <a href="/#">Learn More</a>
+                  </li>
+                  <li>
+                    <a href="/#">Sign In</a>
+                  </li>
+                </ul>
+                <div>
+                  <a href="/#" className={styles.yellowButton}>
+                    Request a demo
+                  </a>
+                </div>
+              </nav>
+            </header>
+            Section 1
+          </div>
+        </section>
+        <section>Section 2</section>
+      </main>
+      <footer>Footer</footer>
+    </>
   );
 }
